5b6b9601a5740580ea0d7c3087aaa9327d4776f0
sannfred <emptynoise21@gmail.com>
2018-11-22 17:42:56 +0200

    I've made hometask 1, 2, with Name Age Location for task 1 and ariphmetical task with 2 numbers

---
+++ task1.py
@@ -0,0 +1,9 @@
+
+name = input("What's your name? ")
+age = input("How old are you? ")
+place = input("Where are you living now?")
+print("Thank you for info:)")
+print("Your name is", name)
+print("Your age is", age)
+print("You're living in", place)
+
---
+++ task2.py
@@ -0,0 +1,12 @@
+number_a = input("Input your first number please")
+
+number_b = input("Input your second number please")
+
+sum = int(number_a) + int(number_b)
+ded = int(number_a) - int(number_b)
+mult = int(number_a) * int(number_b)
+segm = int(number_a) / int(number_b)
+print("number_a + number_b = ", sum)
+print("number_a - number_b = ", ded)
+print("number_a * number_b = ",mult)
+print("number_a / number_b = ",  segm)
